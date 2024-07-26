2b2bd3da34fe4dcec029431d1f0a034ce3d0a4d6
Mohammad Rafly <mohammadrafly19@gmail.com>
2024-07-27 04:53:07 +0700

    adding sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
-use Illuminate\Auth\Middleware\Authenticate;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\ArtistController;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware(Authenticate::using('sanctum'));
+Route::prefix('auth/v1')->group(function() {
+    Route::controller(AuthController::class)->group(function() {
+        Route::post('login', 'login');
+    });
+});
 
-Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
-Route::apiResource('/artist', App\Http\Controllers\Api\ArtistController::class);
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('posts', PostController::class);
+    Route::resource('artist', ArtistController::class);
+    Route::controller(AuthController::class)->group(function() {
+        Route::get('auth/v1/logout', 'logout');
+    });
+});
