9410c32c3460ee9cae31d3c989335b1f5e6cdeef
Mohammad Rafly <mohammadrafly19@gmail.com>
2024-07-30 13:23:27 +0700

    adding custom middleware

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\ArtistController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Middleware\CustomAuthenticate;
 
 Route::prefix('auth/v1')->group(function() {
     Route::controller(AuthController::class)->group(function() {
         Route::post('login', 'login');
     });
-    Route::middleware('auth:sanctum')->group( function () {
+    Route::middleware(CustomAuthenticate::class)->group( function () {
         Route::resource('posts', PostController::class);
         Route::resource('artist', ArtistController::class);
         Route::controller(AuthController::class)->group(function() {
